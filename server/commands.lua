840b1a26d7fd8a97659943720fe407fd7d49a391
Space V <40030799+TheSpaceGamerV2@users.noreply.github.com>
2020-01-22 13:18:37 +0100

    Fixed benzon typo

--- server/commands.lua
+++ server/commands.lua
@@ -117,7 +117,7 @@ if xPlayer then
 	if amount then
 		if account == 'cash' then
 				xPlayer.addMoney(amount)
-			else if xPlayer.getAccount(account) then
+			elseif xPlayer.getAccount(account) then
 			xPlayer.addAccountMoney(account, amount)
 		else
 				TriggerClientEvent('esx:showNotification', source, _U('invalid_account'))
